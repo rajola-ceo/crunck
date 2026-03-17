147e20d25283fed1185590fd46e241f14fe86d7b
rajola-ceo <v3nocyber@gmail.com>
2026-03-17 04:31:33 -0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -77,10 +77,6 @@ function createMovieCard(movie){
       <div class="movie-title">${movie.title}</div>
       <div class="movie-sub">${movie.release_date || "N/A"}</div>
     </div>
-      <div class="card-buttons">
-    <a class="watch-btn" onclick='openVideoPopup(${JSON.stringify(movie)})'>▶ Watch Trailer</a>
-    <button class="watchlist-btn" onclick='addToWatchList(${JSON.stringify(movie)})'>+ Watch List</button>
-  </div>
   `;
   card.addEventListener("click",()=>openPopup(movie));
   return card;
